483e658cc735d6c27366cc591bca68e8d5c1c67c
Gadija Arend <gadijaarend@Gadijas-MacBook-Air.local>
2021-10-24 11:13:08 +0200

    tried out nav bars and nav pills

--- index.js
+++ index.js
@@ -42,32 +42,24 @@ app.listen(PORT, function () {
   console.log("App started at port :", PORT);
 })
 app.get('/', async function (req, res) {
-    // const counter = await greet.nameCounter()
     res.render("index", {
-    //   counter
     })
   })
   app.get('/eggs', async function (req, res) {
-    // const counter = await greet.nameCounter()
     res.render("eggs", {
-    //   counter
+
     })
   })
   app.get('/bread', async function (req, res) {
-    // const counter = await greet.nameCounter()
     res.render("bread", {
-    //   counter
     })
   })
   app.get('/butter', async function (req, res) {
-    // const counter = await greet.nameCounter()
     res.render("butter", {
-    //   counter
     })
   })
   app.get('/cheese', async function (req, res) {
-    // const counter = await greet.nameCounter()
     res.render("cheese", {
-    //   counter
+    
     })
   })
